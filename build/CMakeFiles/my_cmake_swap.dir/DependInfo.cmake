1d6eeac497eb5663db70a2670b593a1e9a465a06
sciencelight <642761715@qq.com>
2023-04-13 10:21:04 +0800

    ²âÊԣ¬³õʼ°汾

---
+++ build/CMakeFiles/my_cmake_swap.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/C++_Projects/test_github/main.cpp" "CMakeFiles/my_cmake_swap.dir/main.cpp.obj" "gcc" "CMakeFiles/my_cmake_swap.dir/main.cpp.obj.d"
+  "D:/C++_Projects/test_github/swap.cpp" "CMakeFiles/my_cmake_swap.dir/swap.cpp.obj" "gcc" "CMakeFiles/my_cmake_swap.dir/swap.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "swap.h"
+using namespace std;
+
+int main()
+{
+    int a = 2, b = 4;
+    printf("%d %d\n", a, b);
+    swap(a, b);
+    printf("%d %d\n", a, b);
+    return 0;
+}
